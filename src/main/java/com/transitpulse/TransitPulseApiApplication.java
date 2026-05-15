f7a8be025dd6ca109ce1946ec4c07d91700c1e28
grzechuzz <grzechu.rz@gmail.com>
2026-05-15 02:16:48 +0200

    Initial commit

---
+++ src/main/java/com/transitpulse/TransitPulseApiApplication.java
@@ -0,0 +1,13 @@
+package com.transitpulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransitPulseApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransitPulseApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/transitpulse/TransitPulseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transitpulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransitPulseApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
