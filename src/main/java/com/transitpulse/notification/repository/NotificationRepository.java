ed3db6e940dba83bd1b8a1d2a6bdd832fa04e53e
grzechuzz <grzechu.rz@gmail.com>
2026-05-18 22:17:40 +0200

    feat: add notification persistence model

---
+++ src/main/java/com/transitpulse/notification/repository/NotificationRepository.java
@@ -0,0 +1,12 @@
+package com.transitpulse.notification.repository;
+
+import com.transitpulse.notification.entity.Notification;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    List<Notification> findByRecipientIdOrderByCreatedAtDesc(Long recipientId);
+
+    long countByRecipientIdAndReadAtIsNull(Long recipientId);
+}
